1b13816113ddf51507445f9ab5862d50a98a7ca9
Kassio Henrique <kassiohenrique8871@gmail.com>
2024-05-01 01:10:12 -0300

    Desafio Concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia("123456789", "Nokia 4451","123456789", 16);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Bomberman");
+
+Iphone iphone = new Iphone("98765", "Iphone 15 ProMax", "98765", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
+//Implementado...
